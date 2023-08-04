73bdd819f89dc71f5956a63f48bef6e6794be575
Adelakun Abayomi <adelakunabayomi123@gmail.com>
2023-08-04 18:37:58 +0100

    build commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,6 @@ function App() {
   }, []);
 
   useEffect(() => {
-    // When the component mounts and isLoading changes to false, add the class to the content area
     if (!isLoading) {
       contentRef.current.classList.add('fade-in');
     }
